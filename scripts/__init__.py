727c3d0e067390f122650046830544eae82081d9
Ubuntu <azureuser@ringdown-analysis-3.gz5ck2jak1vuvndgmdjb2sjnte.cx.internal.cloudapp.net>
2023-02-21 07:25:20 +0000

    added changes to __init__ to prevent clash of InjectionTasks

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,4 +1,4 @@
 from .Injections import *
 from .fit_funcs import *
 from .Posterior import *
-from .TaskFiles import * 
+#from .TaskFiles import *     # Removed to prevent clash of the "InjectionTasks" class
